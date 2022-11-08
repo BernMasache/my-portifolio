bc369dd5c79de0a5e8d62fdf3e3fbbfe78e66c37
BernMasache <bernardmasache@gmail.com>
2022-11-08 08:08:14 +0200

    page setup or portifolio

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,ts,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
